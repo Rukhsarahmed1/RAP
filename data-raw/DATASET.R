42a9938eec02704819a8745fa1cdd888671b7803
Rukhsar Ahmed <ahmedrukhsat2020@gmail.com>
2023-01-22 20:54:38 +0100

    first commit

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,4 @@
 ## code to prepare `DATASET` dataset goes here
 insurance <- readr::read_csv("https://raw.githubusercontent.com/Rukhsarahmed1/RAP/main/insurance.csv")
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(insurance, overwrite = TRUE)
