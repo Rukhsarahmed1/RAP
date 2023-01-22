4e9f209045510055ea93d083f9a93ab923b99401
Rukhsar Ahmed <ahmedrukhsat2020@gmail.com>
2023-01-22 19:18:44 +0100

    done

--- R/Data.R
+++ R/Data.R
@@ -1,1 +1,17 @@
+#' Medical Insurance Dataset
+#'
+#' A tidy dataset of Medical cost personal.
+#'
+#' @format A data frame with 1338 rows and 7 columns:
+#' \describe{
+#'  \item{age}{Age of the Primary beneficiary
+#'  \item{sex}{: Insurance contractor’s gender}
+#'  \item{bmi}{ Insurance contractor’s bmi(body mass index).}
+#'  \item{Children}{The number of the children(dependents) covered by health insurance.}
+#'  \item{smoker}{ Is the insurance contractor’s smoking or not}
+#'  \item{region}{Region shows residential area in United States (US)}
+#'  \item{Charges}{The total charges for medical expenses incurred, as billed by the health insurance}
+#' @source <https://www.kaggle.com/datasets/mirichoi0218/insurance?resource=download>
+
+#'
 "insurance"
--- R/function.R
+++ R/function.R
@@ -1,8 +1,23 @@
+#' INSURANCE dataset
+#'@param insurance A dataframe consisting the information about the insurance.We only select those person whose charges are  more than 0.
+#'@param col1_of_interest The user can select a column of their choice to check different things about the insured person
+#'@param col2_of_interest The users wish is granted and a second column can be chosen to be investigated.
+#'@importFrom janitor clean_names
+#'@importFrom dplyr select filter
+#'@importFrom readr read_csv
+#'@export
+#'@return A simplified dataframe
+#'@details
+#'This function returns a very simplified version of the original dataset, such that the person who is interested in studying the insurance dataset is not
+#'overwhelmed
+#'@examples
+#'clean_insurance_data(insurance, age, charges)
+
 clean_insurance_data <- function(insurance, col1_of_interest, col2_of_interest){
 
   insurance |>
     janitor::clean_names() |>
     dplyr::filter(charges > 0) |>
-    dplyr::select(age, sex, bmi, children, smoke, region, charges, {{col1_of_interest}}, {{col2_of_interest}})
+    dplyr::select(age, sex, bmi, children, smoker, region, charges, {{col1_of_interest}}, {{col2_of_interest}})
 
 }
